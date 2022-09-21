d3b7383b434c61b899eb98d2ea81d865290161c8
Rositsa Miteva <113831422+RositsaMiteva@users.noreply.github.com>
2022-09-21 14:49:40 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+            DateTime plusmin = new DateTime(1994, 6, 16, hours, min, 0);
+            plusmin = plusmin.AddMinutes(30);
+            Console.WriteLine(plusmin.Hour + ":"+ plusmin.Minute); ;
+
+             Console.ReadLine();
+        }
+    }
+}

58a79d5e0aaf5f0af16af0c8ac112fb7cbc9eea8
Rositsa Miteva <113831422+RositsaMiteva@users.noreply.github.com>
2022-09-21 14:51:31 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 
-namespace BackIn30Minutes
+namespace Vacation3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int hours = int.Parse(Console.ReadLine());
-            int min = int.Parse(Console.ReadLine());
-            DateTime plusmin = new DateTime(1994, 6, 16, hours, min, 0);
-            plusmin = plusmin.AddMinutes(30);
-            Console.WriteLine(plusmin.Hour + ":"+ plusmin.Minute); ;
+            int countpeople = int.Parse(Console.ReadLine());
+            string typeofthegroup = Console.ReadLine();
+            string thedayoftheweek = Console.ReadLine();
 
-             Console.ReadLine();
+
+
+
+
+
         }
     }
 }
